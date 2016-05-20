9a9820ffef75a89eccfea805aecedf73d45f7a4e
nnormandin <nicholas.e.normandin@gmail.com>
2016-05-20 17:23:11 +0200

    Test function

---
+++ R/BinCols.R
@@ -0,0 +1,19 @@
+#' Binary columns function
+#'
+#' This function transforms a continuous target vector into a series of binary target vectors
+#' @param x The column to be binarized
+#' @param increment The increment for Equally Probable intervals, defaults to 0.01
+#' @keywords binary
+#' @export
+#' @examples
+#' BinCols(trainingY, increment = 0.02)
+#'
+
+
+BinCols <- function(x, increment = 0.01){
+  prob.values <- seq(from = increment, to = 1-increment, by = increment)
+  y.values <- quantile(x, probs = prob.values)
+  binary.cols <- as.data.frame(lapply(y.values, function(n) ifelse(x >= n, "UP", "DOWN")))
+  output <- list(prob.values, y.values, binary.cols)
+  names(output) <- c("prob.values", "y.values", "columns")
+  return(output)}
