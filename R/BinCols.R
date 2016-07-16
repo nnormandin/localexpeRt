cd3af2fb1bb6ab24dd535323b888b6c878857a41
nnormandin <nicholas.e.normandin@gmail.com>
2016-07-16 12:04:18 -0400

    Fix BinCols function, test TrainLEs

--- R/BinCols.R
+++ R/BinCols.R
@@ -11,19 +11,31 @@
 #' BinCols(y, n = 50, mode = 'EP')
 #'
 
-BinCols <- function(x, n = 30, mode = 'EW'){
 
+BinCols <- function(x, n = 30, mode = 'EW', EW.buffer = 0.02){
+
+  # binarize columns with equal quantiles
   if(mode == 'EP'){
+
+    # set quantile values from 0 to 1; save quantiles and corresponding y-value
     increment = (1/ (n +1))
     prob.values <- seq(from = increment, to = 1-increment, by = increment)
     y.values <- quantile(x, probs = prob.values)
-    binary.cols <- as.data.frame(lapply(y.values, function(i) ifelse(x >= i, "UP", "DOWN")))
-    output <- list(prob.values, y.values, binary.cols)
-    names(output) <- c("prob.values", "y.values", "columns")}
 
+    # apply indicator function, generating binary column for each quantile
+    binary.cols <- as.data.frame(lapply(y.values, function(i) ifelse(x >= i, "UP", "DOWN")))
+
+    # output quantiles, y-values, and list of columns
+    output <- list(prob.values, y.values, binary.cols)
+    names(output) <- c("q.vals", "y.vals", "cols")}
+
+  # binarize columns with equal distance in response variable
   if(mode == 'EW'){
-    y.values <- seq(from = quantile(x, probs = 0.02),
-                    to = quantile(x, probs = 0.98), length.out = n)
+
+    # save
+    y.values <- seq(from = 0),
+                    to = 1, length.out = n + 2)
+    y.values <- y.values[2:length(y.values - 1)]
     binary.cols <- as.data.frame(lapply(y.values, function(i) ifelse(x >= i, "UP", "DOWN")))
     output <- list(y.values, binary.cols)
     names(output) <- c("y.values", "columns")}
