189343e0cc8417349998e1db4ec5c55a797058d5
nnormandin <nicholas.e.normandin@gmail.com>
2016-08-21 19:13:46 -0400

    Update documention and vignette to work with FitInstance changes

--- R/PredictNew.R
+++ R/PredictNew.R
@@ -5,7 +5,6 @@
 #' @param LE.model.list Model list object from TrainLEs function
 #' @param stack.model Regression model trained on LE output
 #' @param y.vals Y-values from BinCols function
-#' @param mode Choose whether probability output is in CDF or PDF format
 #' @param plot Display plot of LE predictions and stack estimate
 #' @param verbose Determines whether prediction information is printed
 #' @param df Degrees of freedom to pass spline if plotting
@@ -15,7 +14,7 @@
 #' @examples
 #'
 
-PredictNew <- function(x, LE.model.list, stack.model, y.vals, mode = 'CDF',
+PredictNew <- function(x, LE.model.list, stack.model, y.vals,
                        plot = FALSE, verbose = FALSE, df = 10, ...){
 
   # 1) pass x vector through all LEs to get prediction
@@ -24,7 +23,7 @@ PredictNew <- function(x, LE.model.list, stack.model, y.vals, mode = 'CDF',
 
   # 2) input LE prediction vector into FitInstance function
 
-  instance.fit <- FitInstance(row = LE.preds, y.values = y.vals, mode = mode, ...)
+  instance.fit <- FitInstance(LE.preds = LE.preds, y.values = y.vals, ...)
 
   # 3) attach meta features if they are detected in the original model
 
@@ -35,24 +34,6 @@ PredictNew <- function(x, LE.model.list, stack.model, y.vals, mode = 'CDF',
       print(paste('Attaching meta-feature:', meta.names[i]))
     }
   }
-
-  ## no longer necessary:
-  # if(mean == TRUE){
-  #   LE.preds <- c(LE.preds, instance.fit$mean)
-  # }
-  #
-  # if(var == TRUE){
-  #   LE.preds <- c(LE.preds, instance.fit$var)
-  # }
-  #
-  # if(skewness == TRUE){
-  #   LE.preds <- c(LE.preds, instance.fit$skew)
-  # }
-  #
-  # if(kurtosis == TRUE){
-  #   LE.preds <- c(LE.preds, instance.fit$kurtosis)
-  # }
-
 
   # 4) transpose and pass new x vector into L1 regression model
   x1 <- t(x1)
